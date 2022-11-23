8d0fc934dbfc87c17be6e1b679a53af6e76d09ea
LikDan <likdan.company@gmail.com>
2022-11-23 11:50:53 +0300

    add proxy

--- apps/social-network/src/app/components/home/home.component.ts
+++ apps/social-network/src/app/components/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'web-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(private http: HttpClient) {
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  tryProxy() {
+    this.http.get("/api/static").subscribe({
+      next: console.log
+    })
+  }
+}
