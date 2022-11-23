b5728bbb7664741ba4d4124f72151e180e91406e
LikDan <likdan.company@gmail.com>
2022-11-23 11:54:54 +0300

    Update ts config

--- apps/social-network/src/app/components/home/home.component.ts
+++ apps/social-network/src/app/components/home/home.component.ts
@@ -11,7 +11,6 @@ export class HomeComponent {
   constructor(private http: HttpClient) {
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   tryProxy() {
     this.http.get("/api/static").subscribe({
       next: console.log
