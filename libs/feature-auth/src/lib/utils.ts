9520a603d33df5d76c178236b7af473b750c76fd
LikDan <likdan.company@gmail.com>
2022-12-05 15:45:01 +0300

    Fix validator

--- libs/feature-auth/src/lib/utils.ts
+++ libs/feature-auth/src/lib/utils.ts
@@ -1,17 +1,6 @@
 import {AbstractControl, ValidationErrors} from "@angular/forms"
-import {Subscription} from "rxjs"
 
-export function sameAs(controlName: string) {
-  let subscription: Subscription | undefined
-
-  return (group: AbstractControl): ValidationErrors | null => {
-    if (subscription == undefined)
-      subscription = group.parent?.get(controlName)?.valueChanges.subscribe((value: string) => {
-        if (group.value == value) delete group.errors?.["notSame"]
-        else group.setErrors({...group.errors, notSame: true})
-      })
-
-    const value = group.parent?.get(controlName)?.value
-    return group.value === value ? null : {notSame: true}
-  }
+export const sameAs = (controlName: string) => (group: AbstractControl): ValidationErrors | null => {
+  const value = group.parent?.get(controlName)?.value
+  return group.value === value ? null : {notSame: true}
 }
